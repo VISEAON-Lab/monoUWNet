c69ece0e04ba631af320cb514ef67a6a456c7bff
shlomi-amitai <shlomi.amitai@gmail.com>
2022-01-20 09:17:23 +0200

    corrLoss weight fix

--- trainer.py
+++ trainer.py
@@ -557,7 +557,7 @@ class Trainer:
 
         # correlation loss
         corrLoss = corr_loss(inputs[("color", 0, 0)], inputs[("color", 0, 0)], outputs[('depth', 0, 0)])
-        total_loss += (0.01*corrLoss)
+        total_loss += (0.1*corrLoss)
 
         losses["loss"] = total_loss 
         return losses
